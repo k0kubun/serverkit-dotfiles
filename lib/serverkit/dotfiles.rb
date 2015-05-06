3d25c2b17f55864cf9c2116be9e7dadb124cb130
Takashi Kokubun <takashikkbn@gmail.com>
2015-05-06 17:27:11 +0900

    Implement dotfiles resource

--- lib/serverkit/dotfiles.rb
+++ lib/serverkit/dotfiles.rb
@@ -1,7 +1,2 @@
 require "serverkit/dotfiles/version"
-
-module Serverkit
-  module Dotfiles
-    # Your code goes here...
-  end
-end
+require "serverkit/resources/dotfiles"
---
+++ lib/serverkit/resources/dotfiles.rb
@@ -0,0 +1,38 @@
+require "serverkit/resources/base"
+
+module Serverkit
+  module Resources
+    # A resource class for Mac OS X user defaults system.
+    class Dotfiles < Base
+      attribute :directory, required: true, type: String
+
+      def apply
+        dotfiles.each do |file|
+          run_command("ln -fs #{::File.join(directory, file)} #{::File.join(ENV["HOME"], file)}")
+        end
+      end
+
+      # @note Override
+      # @return [true, false]
+      def check
+        dotfiles.all? do |file|
+          check_command_from_identifier(
+            :check_file_is_linked_to,
+            ::File.join(ENV["HOME"], file),
+            ::File.join(directory, file),
+          )
+        end
+      end
+
+      private
+
+      def directory
+        ::File.expand_path(attributes["directory"])
+      end
+
+      def dotfiles
+        @dotfiles ||= run_command("ls -A #{directory}").stdout.split("\n")
+      end
+    end
+  end
+end
